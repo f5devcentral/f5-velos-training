d1d57b0b2037d64ccbb37c0ca050f2727a253c1a
Jim McCarron <j.mccarron@f5.com>
2024-01-10 17:50:59 -0500

    test2

--- docs/conf.py
+++ docs/conf.py
@@ -241,7 +241,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, '%s.tex' % cleanname, u'%s Documentation' % classname,
-     u'F5, Inc.', 'manual', False),
+     u'F5, Inc.', 'manual', True),
 ]
 
 # -- Options for manual page output ---------------------------------------
